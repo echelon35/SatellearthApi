02f8b51acaf287ca4326157b9a4168c87b9d970a
echelon35 <kbrun.pro@gmail.com>
2024-04-13 23:05:03 +0200

    Count disasters and enable cors

--- src/alea/modules/disaster/disaster.controller.ts
+++ src/alea/modules/disaster/disaster.controller.ts
@@ -5,8 +5,8 @@ import { ApiTags } from '@nestjs/swagger';
 import { IDisasterFilter } from './Interfaces/IDisasterFilter';
 import { Public } from 'src/decorators/public.decorator';
 
-@ApiTags('alea/disaster')
-@Controller('alea/disaster')
+@ApiTags('disasters')
+@Controller('disasters')
 export class DisasterController {
   constructor(private disasterService: DisasterService) {}
 
@@ -16,5 +16,13 @@ export class DisasterController {
     console.log(query);
     const disasters = await this.disasterService.findAll(query);
     return disasters;
+  }
+
+  @Public()
+  @Get('/count')
+  async count(@Query() query: IDisasterFilter): Promise<number> {
+    console.log(query);
+    const disasters_count = await this.disasterService.count(query);
+    return disasters_count;
   }
 }
--- src/alea/modules/disaster/disaster.service.ts
+++ src/alea/modules/disaster/disaster.service.ts
@@ -13,7 +13,6 @@ export class DisasterService {
   ) {}
 
   async findAll(params: IDisasterFilter): Promise<Disaster[]> {
-    console.log(params);
     const page = params.page || 1;
     const limit = params.limit || 10;
     const sense_order = params.sense_order || 'DESC';
@@ -93,6 +92,80 @@ export class DisasterService {
       limit: limit,
       offset: condition_offset,
       order: [[field_order, sense_order]],
+    });
+  }
+
+  async count(params: IDisasterFilter): Promise<number> {
+    const condition_from_1 = params.from
+      ? { premier_releve: { [Op.gte]: new Date(`${params.from}`) } }
+      : null;
+    const condition_from_2 = params.from
+      ? { dernier_releve: { [Op.gte]: new Date(`${params.from}`) } }
+      : null;
+    const condition_to_1 = params.to
+      ? {
+          premier_releve: {
+            [Op.lte]: new Date().setDate(
+              new Date(`${params.to}`).getDate() + 1,
+            ),
+          },
+        }
+      : null;
+    const condition_to_2 = params.to
+      ? {
+          dernier_releve: {
+            [Op.lte]: new Date().setDate(
+              new Date(`${params.to}`).getDate() + 1,
+            ),
+          },
+        }
+      : null;
+
+    const q_bbox =
+      params.sw_lon && params.sw_lat && params.ne_lon && params.ne_lat
+        ? fn(
+            'ST_MakeEnvelope',
+            params.sw_lon,
+            params.sw_lat,
+            params.ne_lon,
+            params.ne_lat,
+            '4326',
+          )
+        : null;
+
+    const pointDisaster = fn('ST_SetSRID', col('point'), '4326');
+    const intersects =
+      q_bbox != null ? fn('ST_Intersects', q_bbox, pointDisaster) : null;
+    const condition_intersects =
+      q_bbox != null ? where(intersects, true) : null;
+
+    //Recherche de l'id du type d'aléa
+    let alea: Alea;
+    if (params.type != null) {
+      alea = await Alea.findOne({ where: { name: `${params.type}` } });
+    }
+    const condition_type = alea ? { aleaId: `${alea.id}` } : null;
+
+    const condition_fromto =
+      params.from || params.to
+        ? {
+            [Op.or]: [
+              { [Op.and]: [condition_from_1, condition_to_1] },
+              { [Op.and]: [condition_from_2, condition_to_2] },
+            ],
+          }
+        : null;
+
+    //TODO : Make a repository instead
+    return this.disasterModel.count({
+      where: {
+        [Op.and]: [
+          condition_fromto,
+          condition_type,
+          // condition_visible
+          condition_intersects,
+        ],
+      },
     });
   }
 }
